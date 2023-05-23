a5e42491c239eff54583a99d9481798d26db6960
Alexey Tishenkov <grfoger@gmail.com>
2023-05-23 14:37:05 +0300

    add eslint and base aggregate

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'deprecation'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    // 'max-len': [1, { code: 100 }],
+    '@typescript-eslint/lines-between-class-members': 'error',
+    '@typescript-eslint/lines-between-class-members': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'enumMember',
+        format: ['UPPER_CASE'],
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-duplicate-enum-values': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'off',
+	'deprecation/deprecation': 'error'
+  },
+};
---
+++ domain/prodlist/domain-model/ProductListAgregate/item-vo.ts
@@ -0,0 +1,5 @@
+class Item {
+  name: string;
+
+  status: boolean;
+}
